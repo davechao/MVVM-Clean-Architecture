24f5dc65bdbf7de82286678060389f05b256b013
dave_chao <dave790602@gmail.com>
2019-11-30 10:32:18 +0800

    refactor auth_interceptor

--- app/src/main/java/com/test/mvvm/model/api/AuthInterceptor.kt
+++ app/src/main/java/com/test/mvvm/model/api/AuthInterceptor.kt
@@ -6,12 +6,30 @@ import com.test.mvvm.model.api.ApiRepository.Companion.AUTHORIZATION
 import com.test.mvvm.model.api.ApiRepository.Companion.X_APP_VERSION
 import com.test.mvvm.model.pref.Pref
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
+import timber.log.Timber
+import java.net.HttpURLConnection
 
 class AuthInterceptor(private val pref: Pref) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        val requestBuilder = chain.request().newBuilder()
+
+        val response = chain.proceed(chain.buildRequest())
+
+        Timber.d("@@: " + response.code())
+
+        return when (response.code()) {
+            HttpURLConnection.HTTP_UNAUTHORIZED -> {
+                //TODO: do refresh token, then new token save to pref.token
+                chain.proceed(chain.buildRequest())
+            }
+            else -> response
+        }
+    }
+
+    private fun Interceptor.Chain.buildRequest(): Request {
+        val requestBuilder = request().newBuilder()
             .addHeader(X_APP_VERSION, BuildConfig.VERSION_NAME)
 
         if (!TextUtils.isEmpty(pref.token.accessToken)) {
@@ -20,9 +38,6 @@ class AuthInterceptor(private val pref: Pref) : Interceptor {
                 .toString()
             requestBuilder.addHeader(AUTHORIZATION, auth)
         }
-
-        val request = requestBuilder.build()
-        return chain.proceed(request)
+        return requestBuilder.build()
     }
-
 }
